1b0b380c647f1110de5ac0affb6eb3e4d0bf88a1
nizan153 <nizan153@gmail.com>
2019-03-11 21:48:49 +0200

    commit

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -3,41 +3,32 @@ folderPath=$1
 program=$2
 arguments="$3"
 value=0
-compilation="PASS"
-memoryLeak="PASS"
-threadRace="PASS"
+compilation="FAIL"
+memoryLeak="FAIL"
+threadRace="FAIL"
 originalPath=`pwd`
 #move dircteory to folder path
-cd ~
 cd $folderPath
 #check if Makefile exist
-if [[ ! -e "./Makefile" ]]; then
-	echo "Makefile does not exist"
-	value=7
-	compilation="FAIL"
-	memoryLeak="FAIL"
-	threadRace="FAIL"
-else
-	#check compilation
-	make > /dev/null 2>&1
+make > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+	compilation="PASS"
+	#check memory leak
+	valgrind --leak-check=full --error-exitcode=1 "./$program" $arguments > /dev/null 2>&1
 	if [[ $? -eq 0 ]]; then
-		#check memory leak
-		valgrind --leak-check=full --error-exitcode=1 "./$program" $arguments > /dev/null 2>&1
-		if [[ ! $? -eq 0 ]]; then
-			#value=$value+2
-			(( value+=2 ))
-			memoryLeak="FAIL"
-		fi
-		#check thread race
-		valgrind --tool=helgrind --error-exitcode=1 "./$program" $arguments > /dev/null 2>&1
-		if [[ ! $? -eq 0 ]]; then
-			(( value+=4 ))
-			threadRace="FAIL"
-		fi
+		memoryLeak="PASS"
+	else
+		(( value+=2 ))
+	fi
+	#check thread race
+	valgrind --tool=helgrind --error-exitcode=1 "./$program" $arguments > /dev/null 2>&1
+	if [[ $? -eq 0 ]]; then
+		threadRace="PASS"
 	else
 		(( value+=1 ))
-		compilation="FAIL"
 	fi
+else
+	(( value = 7 ))
 fi
 
 #print resualts
